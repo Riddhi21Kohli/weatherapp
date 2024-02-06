238539123b5a684a75ee5fab348a9885c821856a
Riddhi21Kohli <134290721+Riddhi21Kohli@users.noreply.github.com>
2024-02-07 03:45:06 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,154 +1,166 @@
-const weatherApi = {
-    key: '4eb3703790b356562054106543b748b2',
-    baseUrl: 'https://api.openweathermap.org/data/2.5/weather'
+const userTab = document.querySelector("[data-userWeather]");
+const searchTab = document.querySelector("[data-searchWeather]");
+const userContainer = document.querySelector(".weather-container");
+
+const grantAccessContainer = document.querySelector(".grant-location-container");
+const searchForm = document.querySelector("[data-searchForm]");
+const loadingScreen = document.querySelector(".loading-container");
+const userInfoContainer = document.querySelector(".user-info-container");
+
+//initially vairables need????
+
+let oldTab = userTab;
+const API_KEY = "d1845658f92b31c64bd94f06f7188c9c";
+oldTab.classList.add("current-tab");
+getfromSessionStorage();
+
+function switchTab(newTab) {
+    if(newTab != oldTab) {
+        oldTab.classList.remove("current-tab");
+        oldTab = newTab;
+        oldTab.classList.add("current-tab");
+
+        if(!searchForm.classList.contains("active")) {
+            //kya search form wala container is invisible, if yes then make it visible
+            userInfoContainer.classList.remove("active");
+            grantAccessContainer.classList.remove("active");
+            searchForm.classList.add("active");
+        }
+        else {
+            //main pehle search wale tab pr tha, ab your weather tab visible karna h 
+            searchForm.classList.remove("active");
+            userInfoContainer.classList.remove("active");
+            //ab main your weather tab me aagya hu, toh weather bhi display karna poadega, so let's check local storage first
+            //for coordinates, if we haved saved them there.
+            getfromSessionStorage();
+        }
+    }
 }
 
-//anonymous function
-//adding event listener key press of enter
-let searchInputBox = document.getElementById('input-box');
-searchInputBox.addEventListener('keypress', (event) => {
-    if (event.keyCode == 13) {
-        // console.log(searchInputBox.value);
-        getWeatherReport(searchInputBox.value);
-        
+userTab.addEventListener("click", () => {
+    //pass clicked tab as input paramter
+    switchTab(userTab);
+});
+
+searchTab.addEventListener("click", () => {
+    //pass clicked tab as input paramter
+    switchTab(searchTab);
+});
+
+//check if cordinates are already present in session storage
+function getfromSessionStorage() {
+    const localCoordinates = sessionStorage.getItem("user-coordinates");
+    if(!localCoordinates) {
+        //agar local coordinates nahi mile
+        grantAccessContainer.classList.add("active");
     }
+    else {
+        const coordinates = JSON.parse(localCoordinates);
+        fetchUserWeatherInfo(coordinates);
+    }
+
+}
+
+async function fetchUserWeatherInfo(coordinates) {
+    const {lat, lon} = coordinates;
+    // make grantcontainer invisible
+    grantAccessContainer.classList.remove("active");
+    //make loader visible
+    loadingScreen.classList.add("active");
+
+    //API CALL
+    try {
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+          );
+        const  data = await response.json();
+
+        loadingScreen.classList.remove("active");
+        userInfoContainer.classList.add("active");
+        renderWeatherInfo(data);
+    }
+    catch(err) {
+        loadingScreen.classList.remove("active");
+        //HW
+
+    }
+
+}
+
+function renderWeatherInfo(weatherInfo) {
+    //fistly, we have to fethc the elements 
+
+    const cityName = document.querySelector("[data-cityName]");
+    const countryIcon = document.querySelector("[data-countryIcon]");
+    const desc = document.querySelector("[data-weatherDesc]");
+    const weatherIcon = document.querySelector("[data-weatherIcon]");
+    const temp = document.querySelector("[data-temp]");
+    const windspeed = document.querySelector("[data-windspeed]");
+    const humidity = document.querySelector("[data-humidity]");
+    const cloudiness = document.querySelector("[data-cloudiness]");
+
+    //fetch values from weatherINfo object and put it UI elements
+    cityName.innerText = weatherInfo?.name;
+    countryIcon.src = `https://flagcdn.com/144x108/${weatherInfo?.sys?.country.toLowerCase()}.png`;
+    desc.innerText = weatherInfo?.weather?.[0]?.description;
+    weatherIcon.src = `http://openweathermap.org/img/w/${weatherInfo?.weather?.[0]?.icon}.png`;
+    temp.innerText = weatherInfo?.main?.temp;
+    windspeed.innertext = weatherInfo?.wind?.speed;
+    humidity.innertext = weatherInfo?.main?.humidity;
+    cloudiness.innerText = weatherInfo?.clouds?.all;
+
+
+}
+
+function getLocation() {
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+    else {
+        //HW - show an alert for no gelolocation support available
+    }
+}
+
+function showPosition(position) {
+
+    const userCoordinates = {
+        lat: position.coords.latitude,
+        lon: position.coords.longitude,
+    }
+
+    sessionStorage.setItem("user-coordinates", JSON.stringify(userCoordinates));
+    fetchUserWeatherInfo(userCoordinates);
+
+}
+
+const grantAccessButton = document.querySelector("[data-grantAccess]");
+grantAccessButton.addEventListener("click", getLocation);
+
+const searchInput = document.querySelector("[data-searchInput]");
+
+searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let cityName = searchInput.value;
+
+    if(cityName === "")
+        return;
+    else 
+        fetchSearchWeatherInfo(cityName);
 })
 
+async function fetchSearchWeatherInfo(city) {
+    loadingScreen.classList.add("active");
+    userInfoContainer.classList.remove("active");
+    grantAccessContainer.classList.remove("active");
 
-//get waether report
-
-function getWeatherReport(city) {
-    fetch(`${weatherApi.baseUrl}?q=${city}&appid=${weatherApi.key}&units=metric`)  // fetch method fetching the data from  base url ...metric is used for unit in celcius......here i am appending the base url to get data by city name .  
-        .then(weather => {   //weather is from api
-            return weather.json(); // return data from api in JSON
-        }).then(showWeaterReport);  // calling showweatherreport function
-
-}
-
-//show weather report
-
-function showWeaterReport(weather) {
-    let city_code=weather.cod;
-    if(city_code==='400'){ 
-        swal("Empty Input", "Please enter any city", "error");
-        reset();
-    }else if(city_code==='404'){
-        swal("Bad Input", "entered city didn't matched", "warning");
-        reset();
+    try {
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+          );
+        const data = await response.json();
+        loadingScreen.classList.remove("active");
+        userInfoContainer.classList.add("active");
+        renderWeatherInfo(data);
     }
-    else{
-
-    // console.log(weather.cod);
-    // console.log(weather);  
-    let op = document.getElementById('weather-body');
-    op.style.display = 'block';
-    let todayDate = new Date();
-    let parent=document.getElementById('parent');
-    let weather_body = document.getElementById('weather-body');
-    weather_body.innerHTML =
-        `
-    <div class="location-deatils">
-        <div class="city" id="city">${weather.name}, ${weather.sys.country}</div>
-        <div class="date" id="date"> ${dateManage(todayDate)}</div>
-    </div>
-    <div class="weather-status">
-        <div class="temp" id="temp">${Math.round(weather.main.temp)}&deg;C </div>
-        <div class="weather" id="weather"> ${weather.weather[0].main} <i class="${getIconClass(weather.weather[0].main)}"></i>  </div>
-        <div class="min-max" id="min-max">${Math.floor(weather.main.temp_min)}&deg;C (min) / ${Math.ceil(weather.main.temp_max)}&deg;C (max) </div>
-        <div id="updated_on">Updated as of ${getTime(todayDate)}</div>
-    </div>
-    <hr>
-    <div class="day-details">
-        <div class="basic">Feels like ${weather.main.feels_like}&deg;C | Humidity ${weather.main.humidity}%  <br> Pressure ${weather.main.pressure} mb | Wind ${weather.wind.speed} KMPH</div>
-    </div>
-    `;
-    parent.append(weather_body);
-    changeBg(weather.weather[0].main);
-    reset();
-    }
-}
-
-
-
-//making a function for the  last update current time 
-
-function getTime(todayDate) {
-    let hour =addZero(todayDate.getHours());
-    let minute =addZero(todayDate.getMinutes());
-    return `${hour}:${minute}`;
-}
-
-//date manage for return  current date
-function dateManage(dateArg) {
-    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-    let year = dateArg.getFullYear();
-    let month = months[dateArg.getMonth()];
-    let date = dateArg.getDate();
-    let day = days[dateArg.getDay()];
-    // console.log(year+" "+date+" "+day+" "+month);
-    return `${date} ${month} (${day}) , ${year}`
-}
-
-// function for the dynamic background change  according to weather status
-function changeBg(status) {
-    if (status === 'Clouds') {
-        document.body.style.backgroundImage = 'url(img/clouds.jpg)';
-    } else if (status === 'Rain') {
-        document.body.style.backgroundImage = 'url(img/rainy.jpg)';
-    } else if (status === 'Clear') {
-        document.body.style.backgroundImage = 'url(img/clear.jpg)';
-    }
-    else if (status === 'Snow') {
-        document.body.style.backgroundImage = 'url(img/snow.jpg)';
-    }
-    else if (status === 'Sunny') {
-        document.body.style.backgroundImage = 'url(img/sunny.jpg)';
-    } else if (status === 'Thunderstorm') {
-        document.body.style.backgroundImage = 'url(img/thunderstrom.jpg)';
-    } else if (status === 'Drizzle') {
-        document.body.style.backgroundImage = 'url(img/drizzle.jpg)';
-    } else if (status === 'Mist' || status === 'Haze' || status === 'Fog') {
-        document.body.style.backgroundImage = 'url(img/mist.jpg)';
-    }
-
-    else {
-        document.body.style.backgroundImage = 'url(img/bg.jpg)';
-    }
-}
-
-//making a function for the classname of icon
-function getIconClass(classarg) {
-    if (classarg === 'Rain') {
-        return 'fas fa-cloud-showers-heavy';
-    } else if (classarg === 'Clouds') {
-        return 'fas fa-cloud';
-    } else if (classarg === 'Clear') {
-        return 'fas fa-cloud-sun';
-    } else if (classarg === 'Snow') {
-        return 'fas fa-snowman';
-    } else if (classarg === 'Sunny') {
-        return 'fas fa-sun';
-    } else if (classarg === 'Mist') {
-        return 'fas fa-smog';
-    } else if (classarg === 'Thunderstorm' || classarg === 'Drizzle') {
-        return 'fas fa-thunderstorm';
-    } else {
-        return 'fas fa-cloud-sun';
-    }
-}
-
-function reset() {
-    let input = document.getElementById('input-box');
-    input.value = "";
-}
-
-// funtion to add zero if hour and minute less than 10
-function addZero(i) {
-    if (i < 10) {
-        i = "0" + i;
-    }
-    return i;
+    
 }
